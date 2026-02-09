7b572609eb62ce3633c89b963fcc09a90afadb85
Aeryn Deutsch <6784243@qq.com>
2026-02-09 14:06:13 +0800

    fix: 确保MainActivity完全符合V2 embedding标准

--- android/app/src/main/kotlin/com/aeryn/deutsch/MainActivity.kt
+++ android/app/src/main/kotlin/com/aeryn/deutsch/MainActivity.kt
@@ -4,7 +4,14 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
+/**
+ * MainActivity for Aeryn-Deutsch
+ * Uses Flutter Android Embedding V2
+ */
 class MainActivity: FlutterActivity() {
+    /**
+     * Configure the Flutter engine with all plugins
+     */
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine)
     }
